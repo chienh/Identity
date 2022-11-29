caeb2b8afa541f5f9b7c710c2d860cca97b1f062
Chien Hsiao <chienh@gmail.com>
2022-11-29 14:48:22 -0300

    Primeiro commit

---
+++ WebApp1/Data/Migrations/20221129165433_CriacaoAluno.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApp1.Data.Migrations
+{
+    public partial class CriacaoAluno : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Aluno",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Telefone = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Aluno", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Aluno");
+        }
+    }
+}
---
+++ WebApp1/Models/Aluno.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApp1.Models
+{
+    public class Aluno
+    {
+
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Telefone { get; set; }
+        public string Email { get; set; }
+
+    }
+}
